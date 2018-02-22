a8cefc660ed4273117504be1fc2c57a3c22b0180
505 <505@academicos.cesde.edu.co>
2018-02-21 19:46:10 -0500

    creacion inicial del proyecto

---
+++ CalculadoraLC/Operaciones.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraLC
+{
+   public class Operaciones
+    {
+        //Atributos
+        public short numUno;
+        public short numDos;
+
+        //Metodos
+        
+        public void sumar(int numeroUno, int numeroDos)
+        {
+            int suma = 0;
+             suma = numeroUno + numeroDos;
+            Console.WriteLine("la suma es: " + suma);
+        }
+        
+    }
+}
---
+++ CalculadoraLC/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraLC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numero Uno:");//capturando datos por consola
+            short nUno = short.Parse(Console.ReadLine());
+
+            Console.WriteLine("Numero Dos:");
+            short nDos = short.Parse(Console.ReadLine());
+
+           // Operaciones realizarOperacion = new Operaciones();
+            var realizarOperacion = new Operaciones();// esto es igual al imstaciar un objeto de una clase cualquiera
+
+            realizarOperacion.numUno = nUno;
+            realizarOperacion.numDos = nDos;
+
+            //forma de mostrar los atributos de la clase
+            Console.WriteLine(realizarOperacion.numUno + " y " + realizarOperacion.numDos);
+
+            //invocar los metodos
+            realizarOperacion.sumar(realizarOperacion.numUno, realizarOperacion.numDos);
+            Console.ReadLine();
+           
+
+            // realizarOperacion.sumar(nUno, nDos);
+        }
+    }
+}
